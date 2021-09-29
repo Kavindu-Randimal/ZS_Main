91ee66ec9b2bc09bb8b0e5769dec7189f8a0e28a
@randimalg <randimal@zorrosign.com>
2021-09-29 20:54:59 +0530

    first commit

---
+++ iosmobileapplication/DashboardWidget/Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  Bridging-Header.h
+//  DashboardWidgetExtension
+//
+//  Created by Randimal Geeganage on 2021-06-22.
+//  Copyright © 2021 Apple. All rights reserved.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+#import "ConfigurationIntent.h"
+
+
+#endif /* Bridging_Header_h */
